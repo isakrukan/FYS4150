b39c7ee34b4335f43d1322d14edd0f6e86c9d24b
Isak <isak.c.o.rukan@outlook.com>
2025-09-07 15:19:01 +0200

    Problem 2

---
+++ Project1/Code/problem2.cpp
@@ -0,0 +1,63 @@
+#include <iostream> 
+#include <vector> 
+#include <cmath> 
+#include <fstream> 
+#include <iomanip>
+
+// using namespace std;
+
+std::vector<double> linspace(double start, double end, int size=50){
+    // Minimalistic version of np.linspace 
+
+    std::vector<double> x(size);
+    double step_size = (end - start)/(size-1);
+
+    for(int i=0; i<size; i++){
+        x[i] = start + i*step_size;
+    }
+    return x;
+}
+
+std::vector<double> u(std::vector<double> x){
+    // Returns u(x) = 1 - (1 - e^{-10})x - e^{-10x} for a given vector x
+
+    int size = x.size();
+    std::vector<double> u_vector(size);
+    for(int i=0; i<size; i++){
+        u_vector[i] = 1 - (1 - std::exp(-10))*x[i] - exp(-10*x[i]);
+    }
+    return u_vector;
+}
+
+int write_u_to_file(std::string filename, int N, int n_decimals=15){
+    // Creates vector of size "N" x of values from 0 to 1, computes u(x) and writes to "filename". 
+    // Uses "n_decimals" number of decimal points
+
+    std::vector<double> x = linspace(0,1,N);
+    std::vector<double> u_vector = u(x);
+
+    // Creating an output file stream
+    std::ofstream ofile;
+    ofile.open(filename);
+
+    // Writing to file
+    ofile << "x, u, " << N << std::endl; // Header
+    for(int i=0; i<N; i++){
+        ofile << std::setprecision(n_decimals) << std::scientific << x[i];
+        ofile << ", ";
+        ofile << std::setprecision(n_decimals) << std::scientific << u_vector[i] << std::endl;
+    }
+
+    // Close the output file
+    ofile.close();
+
+    return 0;
+}
+
+int main(){
+    int N = 10000;
+    std::string filename = "problem2.txt";
+    write_u_to_file(filename, N);
+
+    std::cout << "2 x " << N << " data points written to " << filename << " complete." << std::endl;
+}
