80f1d5d0f86af42023f1559a6816d0197e86f972
Magnus Axelsen <magnuax@student.matnat.uio.no>
2025-09-17 16:22:22 +0200

    Added parser for command line arguments

---
+++ Project2/Code/include/arg_parser.hpp
@@ -0,0 +1,30 @@
+#ifndef ARG_PARSER_CPP
+#define ARG_PARSER_CPP
+
+#include <string>
+#include <iostream>
+
+/**
+ * Struct to hold command-line arguments
+ */
+struct Args
+{
+    std::string outfile = "build/outfile.csv";
+    bool run_tests = false;
+    bool run_problem_5 = false;
+    bool run_problem_6 = false;
+    double tol = 1e-8;
+    int n_steps = 10;
+    int N_max = 100;
+    int maxiter = 10000;
+};
+
+/**
+ * \brief Function for parsing command-line arguments
+ * @param argc Number of command-line arguments
+ * @param argv Array of command-line argument strings
+ * @return Args struct containing the parsed values
+ */
+Args parse_args(int argc, char *argv[]);
+
+#endif
